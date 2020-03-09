b94a7d67dd8ff5efdd0793a192d1caabff4eb195
Nick Comly <ncomly@stanford.edu>
2020-03-08 22:25:40 -0700

    added .gitignore + second round of augmentation run with bert script and generator

---
+++ scripts/bert_bash_generator.py
@@ -0,0 +1,13 @@
+import os
+output_dir = "./"
+output = "aug2_bert_train.sh"
+
+files = os.listdir('../data/aug2_data/')
+# remove extensions
+files = [f.split('.')[0] for f in files]
+
+with open(output_dir + output, 'w') as f:
+    f.write("#!/bin/bash\n")
+    for fi in files:
+        f.write(f"python ../train/bert_train.py --dataset=aug2_data/{fi}\n")
+
