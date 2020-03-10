1803a93d6c1920c805288693eaa6a0b9bfc77eca
dwkang@stanford.edu <dkang9503@gmail.com>
2020-03-10 09:51:04 -0700

    changed some script names

---
+++ scripts/bert_fix_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+python ../train/bert_train.py --dataset=rd_alpha_0.1_num_aug_1
+python ../train/bert_train.py --dataset=ri_alpha_0.1_num_aug_1
+python ../train/bert_train.py --dataset=rs_alpha_0.1_num_aug_1
+python ../train/bert_train.py --dataset=sr_alpha_0.1_num_aug_1
+python ../train/bert_train.py --dataset=rd_alpha_0.3_num_aug_1
+python ../train/bert_train.py --dataset=rs_alpha_0.3_num_aug_1
+python ../train/bert_train.py --dataset=sr_alpha_0.3_num_aug_1
+python ../train/bert_train.py --dataset=rd_alpha_0.5_num_aug_1
+python ../train/bert_train.py --dataset=ri_alpha_0.5_num_aug_1
+python ../train/bert_train.py --dataset=rs_alpha_0.5_num_aug_1
+python ../train/bert_train.py --dataset=sr_alpha_0.5_num_aug_1
+python ../train/bert_train.py --dataset=ri_alpha_0.1_num_aug_3
+python ../train/bert_train.py --dataset=rs_alpha_0.1_num_aug_3
+python ../train/bert_train.py --dataset=sr_alpha_0.1_num_aug_3
+python ../train/bert_train.py --dataset=rd_alpha_0.3_num_aug_3
+python ../train/bert_train.py --dataset=ri_alpha_0.3_num_aug_3
+python ../train/bert_train.py --dataset=rs_alpha_0.3_num_aug_3
+python ../train/bert_train.py --dataset=sr_alpha_0.3_num_aug_3
+python ../train/bert_train.py --dataset=rd_alpha_0.5_num_aug_3
+python ../train/bert_train.py --dataset=sr_alpha_0.5_num_aug_3
+python ../train/bert_train.py --dataset=rd_alpha_0.1_num_aug_5
+python ../train/bert_train.py --dataset=ri_alpha_0.1_num_aug_5
+python ../train/bert_train.py --dataset=rs_alpha_0.1_num_aug_5
+python ../train/bert_train.py --dataset=sr_alpha_0.1_num_aug_5
+python ../train/bert_train.py --dataset=rd_alpha_0.3_num_aug_5
+python ../train/bert_train.py --dataset=ri_alpha_0.3_num_aug_5
+python ../train/bert_train.py --dataset=rs_alpha_0.3_num_aug_5
+python ../train/bert_train.py --dataset=sr_alpha_0.3_num_aug_5
+python ../train/bert_train.py --dataset=rd_alpha_0.5_num_aug_5
+python ../train/bert_train.py --dataset=ri_alpha_0.5_num_aug_5
