b7bab64d914591242d3a5bbe818ba925de50e8f6
Nick Comly <ncomly@stanford.edu>
2020-03-11 22:05:31 -0700

    moved things around and generated more datasets

--- utils/aug2_scripts/word_aug_bash_script.sh
+++ utils/aug2_scripts/word_aug_bash_script.sh
@@ -11,3 +11,6 @@ python word_aug_script.py --input=../train.csv --da_type=contextsub --num_aug=1 
 python word_aug_script.py --input=../train.csv --da_type=contextsub --num_aug=1 --model_dir=../../models/saved_models/ --output_dir=../aug2_data/ --alpha=0.3
 python word_aug_script.py --input=../train.csv --da_type=contextsub --num_aug=3 --model_dir=../../models/saved_models/ --output_dir=../aug2_data/ --alpha=0.1
 python word_aug_script.py --input=../train.csv --da_type=contextsub --num_aug=3 --model_dir=../../models/saved_models/ --output_dir=../aug2_data/ --alpha=0.3
+
+
+# python word_aug_script.py --input=../../data/aug2_data/contextsub_alpha_0.1_num_aug_1.csv --da_type=glovetwit --num_aug=1 --model_dir=../../models/saved_models/ --output_dir=../../data/aug2_data/multiple --alpha=0.3
